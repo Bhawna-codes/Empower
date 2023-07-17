28de36400ca9abf83e8e8f136b114c570dd842eb
BHAWNA CHAUDHRY <bhawnachaudhry57@gmail.com>
2023-07-17 15:59:08 +0530

    Revert "Merge remote-tracking branch 'upstream/Development' into Development"
    This reverts commit 0dcafb3b81a020df3168f26b29ee5b2c12543cbe, reversing
    changes made to d5d1a3edb3ff08622418047adbaf708ce4905429.

--- src/examples/Navbars/DashboardNavbar/ChangePassWord.js
+++ src/examples/Navbars/DashboardNavbar/ChangePassWord.js
@@ -1,43 +1,16 @@
 import React, { useState } from "react";
-import { TextField, Button, Typography, Box, InputAdornment, IconButton } from "@mui/material";
-import axios from "axios";
 
-import "./ChangePass.css";
+import { TextField, Button, Typography, Box } from "@mui/material";
+
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { Visibility, VisibilityOff } from "@mui/icons-material";
-const validatePassword = (password) => {
-  const numberRegex = /\d/;
-  const capitalLetterRegex = /[A-Z]/;
-  const specialCharacterRegex = /[!@#$%^&*()]/;
-  let trimmedPassword = password.trim();
-  let errors = [];
-  if (password !== trimmedPassword) {
-    errors.push(<p className="textsize"> Extra spaces are not allowed in the password.</p>);
-  }
-  if (!numberRegex.test(trimmedPassword)) {
-    errors.push(<p className="textsize">Password must contain at least one number. </p>);
-  }
-  if (!capitalLetterRegex.test(trimmedPassword)) {
-    errors.push(<p className="textsize">Password must contain at least one capital letter.</p>);
-  }
-  if (!specialCharacterRegex.test(trimmedPassword)) {
-    errors.push(<p className="textsize">Password must contain at least one special character.</p>);
-  }
-  return errors;
-};
+
 const ChangePassWord = () => {
   const [oldPassword, setOldPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState("");
   const [success, setSuccess] = useState(false);
-
-  const [showErrors, setShowErrors] = useState(false);
-
-  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-  const handleToggleConfirmPasswordVisibility = () => {
-    setShowConfirmPassword(!showConfirmPassword);
-  };
   const navigate = useNavigate();
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -45,32 +18,21 @@ const ChangePassWord = () => {
     if (name === "newPassword") setNewPassword(value);
     if (name === "confirmPassword") setConfirmPassword(value);
   };
+
   const authToken = localStorage.getItem("authToken");
   const mydata = JSON.parse(localStorage.getItem("myData"));
+
   console.log(authToken);
   console.log(mydata, "mydata");
+
   let empid = mydata.empCode;
   let emplyee = mydata.employeeName;
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError("");
     setSuccess(false);
-    const passwordValidationErrors = validatePassword(newPassword);
-    if (passwordValidationErrors.length > 0) {
-      setError(passwordValidationErrors[0]);
-      setShowErrors(true);
-      return;
-    }
-    if (newPassword !== confirmPassword) {
-      setError(<p className="textsize">New password and confirm password do not match.</p>);
-      setShowErrors(true);
-      return;
-    }
-    if (newPassword.length < 8) {
-      setError(<p className="textsize">New password must be at least 8 characters long. </p>);
-      setShowErrors(true);
-      return;
-    }
+
     try {
       const response = await axios.post("https://localhost:5001/api/UserAccount/UpdatePassword", {
         empCode: empid,
@@ -79,11 +41,8 @@ const ChangePassWord = () => {
         confirmPassword: confirmPassword,
         modifiedBy: emplyee,
       });
-      console.log(response.data);
 
-      console.log(success);
-
-      setSuccess(response.data);
+      setSuccess(response.data.message);
       setOldPassword("");
       setNewPassword("");
       setConfirmPassword("");
@@ -100,17 +59,18 @@ const ChangePassWord = () => {
       </Typography>
 
       <form onSubmit={handleSubmit}>
-        {showErrors && error && (
+        {error && (
           <Typography variant="body1" color="error" gutterBottom>
             {error}
-            {}
           </Typography>
         )}
+
         {success && (
           <Typography variant="body1" color="success" gutterBottom>
             {success}
           </Typography>
         )}
+
         <TextField
           type="text"
           name="oldPassword"
@@ -121,6 +81,7 @@ const ChangePassWord = () => {
           margin="normal"
           required
         />
+
         <TextField
           type="text"
           name="newPassword"
@@ -131,8 +92,9 @@ const ChangePassWord = () => {
           margin="normal"
           required
         />
+
         <TextField
-          type={showConfirmPassword ? "text" : "password"}
+          type="password"
           name="confirmPassword"
           value={confirmPassword}
           onChange={handleChange}
@@ -140,20 +102,8 @@ const ChangePassWord = () => {
           fullWidth
           margin="normal"
           required
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleToggleConfirmPasswordVisibility}
-                  edge="end"
-                >
-                  {showConfirmPassword ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
         />
+
         <Button type="submit" variant="contained" fullWidth>
           Change Password
         </Button>
@@ -161,4 +111,5 @@ const ChangePassWord = () => {
     </Box>
   );
 };
+
 export default ChangePassWord;
